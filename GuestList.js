ddb2f95410400a350cf98968d0710a6572c9b45b
Shumaila Noreen <161513956+Sharniki098@users.noreply.github.com>
2024-05-20 01:32:30 -0700

    Add files via upload

---
+++ GuestList.js
@@ -0,0 +1,4 @@
+var Gust_List = ["Muskan", "Aria", "Mel"];
+for (var i = 0; i < Gust_List.length; i++) {
+    console.log("Dear Mr." + Gust_List[i] + ",\n\nit is our pleauusr to invite you in our party. \n\nThank You!");
+}
---
+++ GuestList.ts
@@ -0,0 +1,7 @@
+
+
+let Gust_List :string[] =["Muskan","Aria","Mel"];
+
+for(let i=0;  i<Gust_List.length; i++){
+ console.log("Dear Mr." +Gust_List[i] + ",\n\nit is our pleauusr to invite you in our party. \n\nThank You!");   
+}
